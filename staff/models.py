a58be879b1447c6822a99bede95d1b6edb9e79bb
IFURK0N <ifurkan2234@gmail.com>
2024-08-23 17:05:52 +0300

    Not ready for update but needed.

---
+++ staff/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Staff(models.Model):
+    user_name = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
